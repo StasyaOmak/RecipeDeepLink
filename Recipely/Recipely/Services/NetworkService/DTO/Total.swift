f16b714686a46331b389fd2c7153d9cc7477248f
tixomark <tixomark@mail.ru>
2024-03-13 12:37:41 +0300

    feat(networkService): Separated DTO into several files

---
+++ Recipely/Recipely/Services/NetworkService/DTO/Total.swift
@@ -0,0 +1,14 @@
+//
+//  Total.swift
+//  Recipely
+//
+//  Created by Tixon Markin on 13.03.2024.
+//
+
+import Foundation
+
+/// Структура, представляющая общее количество питательного вещества.
+struct Total: Codable {
+    /// Количество питательного вещества.
+    let quantity: Double
+}
