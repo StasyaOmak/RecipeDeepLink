91db67641502730c2fa9aee217a953f63facd1a0
tixomark <tixomark@mail.ru>
2024-03-29 15:02:55 +0300

    feat(app): Created project.yml for XcodeGen.  pbxproj is generated by XcodeGen from now on

--- Recipely/Recipely/Services/CoreDataService/CDDish+CoreDataClass.swift
+++ Recipely/Recipely/Services/CoreDataService/CDDish+CoreDataClass.swift
@@ -5,4 +5,22 @@ import CoreData
 import Foundation
 
 @objc(CDDish)
-public class CDDish: NSManagedObject {}
+/// Класс, представляющий блюдо для CoreData
+final class CDDish: NSManagedObject {
+    convenience init(dish: Dish, context: NSManagedObjectContext) {
+        self.init(context: context)
+        uri = dish.uri
+        linkToImage = dish.linkToImage
+        linkToThumbnailImage = dish.linkToThumbnailImage
+        name = dish.name
+        weight = dish.weight
+        category = dish.category
+        cookingTime = Int16(dish.cookingTime)
+        calories = dish.calories
+        carbohydrates = dish.carbohydrates
+        fats = dish.fats
+        proteins = dish.proteins
+        recipe = dish.recipe
+        isFavourite = dish.isFavourite
+    }
+}
--- Recipely/Recipely/Services/CoreDataService/CDDish+CoreDataProperties.swift
+++ Recipely/Recipely/Services/CoreDataService/CDDish+CoreDataProperties.swift
@@ -4,21 +4,35 @@
 import CoreData
 import Foundation
 
-/// Модель блюда для CoreData
-public extension CDDish {
+extension CDDish {
     @nonobjc class func fetchRequest() -> NSFetchRequest<CDDish> {
         NSFetchRequest<CDDish>(entityName: "CDDish")
     }
 
-    @NSManaged var uri: String?
-    @NSManaged var linkToImage: String?
-    @NSManaged var linkToThumbnailImage: String?
-    @NSManaged var name: String?
-    @NSManaged var weight: Float
-    @NSManaged var cookingTime: Int64
+    /// Энергетическая ценность блюда.
     @NSManaged var calories: Float
+    /// Количество углеводов в блюде.
     @NSManaged var carbohydrates: Float
+    /// Тип блюда
+    @NSManaged var category: String?
+    /// Время приготовления блюда в минутах.
+    @NSManaged var cookingTime: Int16
+    /// Количество жиров в блюде.
     @NSManaged var fats: Float
+    /// Является ли рецепт любимым
+    @NSManaged var isFavourite: Bool
+    /// Сылка на изображение блюда
+    @NSManaged var linkToImage: String
+    /// Сылка на изображение блюда в сжатом формате
+    @NSManaged var linkToThumbnailImage: String
+    /// Название блюда
+    @NSManaged var name: String
+    /// Количество белков в блюде.
     @NSManaged var proteins: Float
-    @NSManaged var recipe: String?
+    /// Рецепт блюда.
+    @NSManaged var recipe: String
+    /// Идентификатор блюда
+    @NSManaged var uri: String
+    /// Вес блюда
+    @NSManaged var weight: Float
 }
